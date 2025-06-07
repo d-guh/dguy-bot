262f9f1d7ea0d53fbdffe1f3e1ad0bd04be2f5aa
D-Guy <ddcool535@gmail.com>
2025-06-07 00:59:45 -0400

    Improve ping and ready

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -6,8 +6,13 @@ module.exports = {
         .setDescription('Pings the bot.'),
     async execute(interaction) {
         try {
-            const latency = Date.now() - interaction.createdTimestamp;
-            await interaction.reply(`Pong! :ping_pong: \`${latency}ms\``);
+            await interaction.reply({ content: 'Pinging...' });
+            const sent = await interaction.fetchReply();
+
+            const roundTrip = sent.createdTimestamp - interaction.createdTimestamp;
+            const wsPing = interaction.client.ws.ping; // Returns -1 if websocket not established
+
+            await interaction.editReply(`Pong! :ping_pong:\nWebSocket Ping: \`${wsPing}ms\`\nRound-trip Latency: \`${roundTrip}ms\``);
         } catch (error) {
             console.error('Error while executing /ping:', error);
         }
